2e0b2060f2d12ad479ac687c1518fe056764833d
Vineetha Devavarapu <devavarapuvineetha04@gmail.com>
2025-05-27 23:35:33 +0530

    Update Techstack.js

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { CgCPlusPlus } from "react-icons/cg";
 import {
   DiJavascript1,
   DiReact,
@@ -9,14 +8,12 @@ import {
   DiJava,
 } from "react-icons/di";
 import {
-  SiFirebase,
   SiPostgresql,
 } from "react-icons/si";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-  
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
@@ -26,7 +23,6 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
-  
       <Col xs={4} md={2} className="tech-icons">
         <SiPostgresql />
       </Col>
